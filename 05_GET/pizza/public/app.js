54e11c6154662d7a46241ecb670a4faf810523e7
Mladen Rudic <RUD220478@spengergasse.at>
2025-11-30 10:39:43 +0100

    Implemented Delete

--- 05_GET/pizza/public/app.js
+++ 05_GET/pizza/public/app.js
@@ -29,7 +29,16 @@ async function fetchOrders() {
       const tdPizza = document.createElement("td");
       tdPizza.textContent = o.pizza;
 
-      tr.append(tdId, tdName, tdPizza);
+      const delBtn = document.createElement("button");
+      delBtn.textContent = "Delete";
+      delBtn.className = "delete-btn";
+      delBtn.onclick = async () => {
+          if (!confirm(`Remove order ${o.name}?`)) return;
+          await deleteOrder(o.id);
+          await fetchOrders();
+        };
+
+      tr.append(tdId, tdName, tdPizza, delBtn);
       tbody.appendChild(tr);
     }
 
@@ -78,6 +87,22 @@ async function addOrder(name, pizza) {
     statusEl.textContent = `Error while added a order: ${err.message}`;
   }
 }
+
+async function deleteOrder(id) {
+  const statusEl = document.getElementById("status");
+  try {
+    const res = await fetch(`/orders/${id}`, { method: "DELETE" });
+    if (res.status === 204) {
+      statusEl.textContent = `Order ${id} removed.`;
+    } else {
+      throw new Error(msg.error || `HTTP ${res.status}`);
+    }
+  } catch (err) {
+    console.error(err);
+    statusEl.textContent = `Error while removing: ${err.message}`;
+  }
+}
+
 
 // When page isloaded the fetchOrders is called
 window.addEventListener("DOMContentLoaded", fetchOrders);
